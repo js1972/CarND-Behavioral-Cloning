2a4d952fa35db2045f272bafcc8583b9bbd07afc
Jason Scott <jason@jaylin.com.au>
2017-01-25 18:08:55 +0800

    Revert "change the model like the comma.ai model"
    This reverts commit 557a091864f21b549d5a074f1287d682d962d398.

--- utils.py
+++ utils.py
@@ -94,52 +94,6 @@ def random_flip(imgs, angles):
     return new_imgs, new_angles
 
 
-def add_random_shadow(image):
-    """
-    Add a random shadow to an image
-    :param image:
-    :return:
-    """
-    top_y = 320*np.random.uniform()
-    top_x = 0
-    bot_x = 160
-    bot_y = 320*np.random.uniform()
-    image_hls = cv2.cvtColor(image.astype("uint8"), cv2.COLOR_RGB2HLS)
-    shadow_mask = 0*image_hls[:,:,1]
-    X_m = np.mgrid[0:image.shape[0],0:image.shape[1]][0]
-    Y_m = np.mgrid[0:image.shape[0],0:image.shape[1]][1]
-
-    shadow_mask[((X_m-top_x)*(bot_y-top_y) -(bot_x - top_x)*(Y_m-top_y) >=0)]=1
-    #random_bright = .25+.7*np.random.uniform()
-
-    if np.random.randint(2)==1:
-        random_bright = .5
-        cond1 = shadow_mask==1
-        cond0 = shadow_mask==0
-        if np.random.randint(2)==1:
-            image_hls[:,:,1][cond1] = image_hls[:,:,1][cond1]*random_bright
-        else:
-            image_hls[:,:,1][cond0] = image_hls[:,:,1][cond0]*random_bright
-
-    image = cv2.cvtColor(image_hls, cv2.COLOR_HLS2RGB)
-    return image
-
-
-def add_random_shadows(imgs):
-    """
-    Add random shadows across the image.
-
-    :param imgs: Numpy array of images
-    :return: Numpy array of images with a random shadow
-    """
-    shadow_imgs = np.empty_like(imgs)
-
-    for i, image in enumerate(imgs):
-        shadow_imgs[i] = add_random_shadow(image)
-
-    return shadow_imgs
-
-
 def augment_brightness(images):
     """
     Randomly adjust brightness of provided images.
@@ -176,8 +130,7 @@ def augment(imgs, angles):
     :param angles: Numpy array of angles
     :return: The augmented images and angles as a tuple
     """
-    augmented_shadow_imgs = add_random_shadows(imgs)
-    augmented_brightness_imgs = augment_brightness(augmented_shadow_imgs)
+    augmented_brightness_imgs = augment_brightness(imgs)
     imgs_augmented, angles_augmented = random_flip(augmented_brightness_imgs, angles)
 
     return imgs_augmented, angles_augmented
